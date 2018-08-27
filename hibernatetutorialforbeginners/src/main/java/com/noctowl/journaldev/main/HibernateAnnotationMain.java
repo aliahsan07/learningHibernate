21b20f3d706ce10fdcd9d4bc7ef41396ae31fa01
Ali Ahsan <aliahsan07@outlook.com>
2018-08-27 19:18:49 +0500

    still not done with the basics

---
+++ hibernatetutorialforbeginners/src/main/java/com/noctowl/journaldev/main/HibernateAnnotationMain.java
@@ -0,0 +1,35 @@
+package com.noctowl.journaldev.main;
+
+import com.noctowl.journaldev.model.Employee1;
+import com.noctowl.journaldev.util.HibernateUtil;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+public class HibernateAnnotationMain {
+
+    public static void main(String[] args) {
+        Employee1 emp = new Employee1();
+        emp.setName("David");
+        emp.setRole("Developer");
+        emp.setInsertTime(new Date());
+
+        //Get Session
+        SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+        Session session = sessionFactory.getCurrentSession();
+        //start transaction
+        session.beginTransaction();
+        //Save the Model object
+        session.save(emp);
+        //Commit transaction
+        session.getTransaction().commit();
+        System.out.println("Employee ID="+emp.getId());
+
+        //terminate session factory, otherwise program won't end
+        sessionFactory.close();
+    }
+
+}
